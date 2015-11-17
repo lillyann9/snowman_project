1acf556863d656eb729a6a9da1f1268a34cd0e40
Lilian Patiño Ortiz <lilly.0608@gmail.com>
2015-11-17 08:03:02 -0600

    Working

--- js/index.js
+++ js/index.js
@@ -199,8 +199,9 @@ var slow = 5
       1 + Math.cos(counter / slow) / scaleShrinkage
     );
    // requestAnimationFrame( render ); 
- 
+
    move();
+ 
 }
 
  render()
@@ -246,7 +247,6 @@ $(window).keypress(function (e) {
 
 function move()
 {
-
     if(movement == 1)
     {
        requestAnimationFrame( render ); 
@@ -262,3 +262,141 @@ camera.position.z = zp;
 camera.lookAt(snowmanMesh.position)
 
 }
+
+// nieve
+
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
+
+var container;
+
+var particle;
+
+var camera;
+var scene;
+var renderer;
+
+var mouseX = 0;
+var mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+var particles = []; 
+var particleImage = new Image(); //THREE.ImageUtils.loadTexture( "http://i.imgur.com/cTALZ.png" );
+particleImage.src = 'http://i.imgur.com/cTALZ.png'; 
+
+function init() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
+    camera.position.z = 1000;
+
+    scene = new THREE.Scene();
+    scene.add(camera);
+        
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    var material = new THREE.ParticleBasicMaterial( { map: new THREE.Texture(particleImage) } );
+        
+    for (var i = 0; i < 500; i++) {
+
+        particle = new Particle3D( material);
+        particle.position.x = Math.random() * 2000 - 1000;
+        particle.position.y = Math.random() * 2000 - 1000;
+        particle.position.z = Math.random() * 2000 - 1000;
+        particle.scale.x = particle.scale.y =  1;
+        scene.add( particle );
+        
+        particles.push(particle); 
+    }
+
+    container.appendChild( renderer.domElement );
+    setInterval(loop, 1000 / 60);    
+}
+
+
+function loop() 
+{
+
+for(var i = 0; i<particles.length; i++) 
+{
+        var particle = particles[i]; 
+        particle.updatePhysics(); 
+
+        with(particle.position) 
+        {
+            if(y<-1000) y+=2000; 
+            if(x>1000) x-=2000; 
+            else if(x<-1000) x+=2000; 
+            if(z>1000) z-=2000; 
+            else if(z<-1000) z+=2000; 
+        }                
+    }
+}
+
+var TO_RADIANS = Math.PI / 180;
+
+Particle3D = function(material) 
+{
+    THREE.Particle.call(this, material);
+
+    //this.material = material instanceof Array ? material : [ material ];
+    // define properties
+    this.velocity = new THREE.Vector3(0, -8, 0);
+    this.velocity.rotateX(randomRange(-45, 45));
+    this.velocity.rotateY(randomRange(0, 360));
+    this.gravity = new THREE.Vector3(0, 0, 0);
+    this.drag = 1;
+    
+};
+
+Particle3D.prototype = new THREE.Particle();
+Particle3D.prototype.constructor = Particle3D;
+
+Particle3D.prototype.updatePhysics = function() {
+    this.velocity.multiplyScalar(this.drag);
+    this.velocity.addSelf(this.gravity);
+    this.position.addSelf(this.velocity);
+}
+
+THREE.Vector3.prototype.rotateY = function(angle) {
+    cosRY = Math.cos(angle * TO_RADIANS);
+    sinRY = Math.sin(angle * TO_RADIANS);
+
+    var tempz = this.z;
+    var tempx = this.x;
+
+    this.x = (tempx * cosRY) + (tempz * sinRY);
+    this.z = (tempx * -sinRY) + (tempz * cosRY);
+}
+
+THREE.Vector3.prototype.rotateX = function(angle) {
+    cosRY = Math.cos(angle * TO_RADIANS);
+    sinRY = Math.sin(angle * TO_RADIANS);
+
+    var tempz = this.z;
+    var tempy = this.y;
+
+    this.y = (tempy * cosRY) + (tempz * sinRY);
+    this.z = (tempy * -sinRY) + (tempz * cosRY);
+}
+
+THREE.Vector3.prototype.rotateZ = function(angle) {
+    cosRY = Math.cos(angle * TO_RADIANS);
+    sinRY = Math.sin(angle * TO_RADIANS);
+
+    var tempx = this.x;
+    var tempy = this.y;
+
+    this.y = (tempy * cosRY) + (tempx * sinRY);
+    this.x = (tempy * -sinRY) + (tempx * cosRY);
+}
+
+// returns a random number between the two limits provided 
+
+function randomRange(min, max) {
+    return ((Math.random() * (max - min)) + min);
+}
+
